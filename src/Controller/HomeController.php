a3e18f9383abd5ca22054beccf7e40ffe175d0a9
Thierry RANDRIANTIANA <thierry1804@gmail.com>
2024-05-19 14:17:58 +0300

    Ajout de index.html.twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,17 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): JsonResponse
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/HomeController.php',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
