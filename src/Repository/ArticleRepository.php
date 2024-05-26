74f063cc8024585d11563cae092ebaaf5293178c
Thierry RANDRIANTIANA <thierry1804@gmail.com>
2024-05-26 21:53:59 +0300

    DA-5 maj stock réel

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -16,6 +16,42 @@ class ArticleRepository extends ServiceEntityRepository
         parent::__construct($registry, Article::class);
     }
 
+    /**
+     * @throws \Doctrine\DBAL\Exception
+     */
+    public function getArticles(): array
+    {
+        $sql = "
+            SELECT
+                a.id,
+                a.label,
+                a.description,
+                a.purchase_price AS 'purchasePrice',
+                a.sell_price AS 'sellPrice',
+                a.min_stock AS 'minStock',
+                COALESCE(m.stock_after, a.in_stock) AS 'inStock',
+                IF(a.min_stock >= COALESCE(m.stock_after, a.in_stock), 'danger', 'success') AS 'status'
+            FROM article a
+            LEFT JOIN (
+                SELECT 
+                    article_id,
+                    stock_after
+                FROM movement
+                WHERE (article_id, id) IN (
+                    SELECT article_id, MAX(id)
+                    FROM movement
+                    GROUP BY article_id
+                )
+            ) m ON a.id = m.article_id
+            ORDER BY a.label ASC;
+        ";
+
+        $conn = $this->getEntityManager()->getConnection();
+        $stmt = $conn->executeQuery($sql);
+
+        return $stmt->fetchAllAssociative();
+    }
+
     //    /**
     //     * @return Article[] Returns an array of Article objects
     //     */
