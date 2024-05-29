f5ada270413e062b249c02023336b7bdd66c460b
Thierry RANDRIANTIANA <thierry1804@gmail.com>
2024-05-29 20:38:54 +0300

    DA-8 début dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,11 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class DashboardController extends AbstractController
 {
-    #[Route('/x', name: 'app_homex')]
+    #[Route('/', name: 'app_dashboard')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
     }
 }
