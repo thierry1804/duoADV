3239fbe25e4a5762aa38606dffcd7665be7bd4ad
Thierry RANDRIANTIANA <b.randriantiana@etechconsulting-mg.com>
2024-06-10 13:22:02 +0300

    Correction registration redirect

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -52,7 +52,7 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('app_dashboard');
         }
 
         return $this->render('registration/register.html.twig', [
